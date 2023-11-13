57c6afe64c620fc716d44006f00eb4e947e247c8
visitor17564 <lanlanloo17564@gmail.com>
2023-11-13 20:31:59 +0900

    Product 마이그레이션 업데이트

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ try {
 
 const app = express();
 const port = 3000;
-const productsRouter = require("./routes/products.router"); // 미들웨어 세팅
+const productsRouter = require("./routes/products.route.js"); // 미들웨어 세팅
+const userRouter = require("./routes/user.route.js"); // 미들웨어 세팅
 const connect = require("./schemas"); // 몽고DB 스키마 연결
 connect();
 
 app.use(express.json()); // json미들웨어 세팅
-app.use("/api", [productsRouter]); // localhost:3000/api -> productsRouter
+app.use("/api", [productsRouter]); // productsRouter 미들웨어
+app.use("/auth", [userRouter]); // userRouter 미들웨어
 
 app.listen(port, () => {
   console.log(port, "포트로 서버가 열렸어요!");
